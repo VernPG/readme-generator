0badcf692fee023870479ceef600f3fb0fae8eba
Veronica Perez <veperez@live.com>
2023-10-22 12:59:49 -0500

    added list of input questions

--- index.js
+++ index.js
@@ -1,9 +1,70 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const fs= require("fs");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+inquirer.prompt([
+  {
+    type: "imput",
+    message: "What is the title of the project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please describe this project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What is the intended use of this project",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Who are the contributors to this project?",
+    name: "contributors",
+  },
+  {
+    type: "input",
+    message: "How do you test the project?",
+    name: "",
+  },
+  {
+    type:"list",
+    message:"What license was selected for this project?",
+    choices:[
+        "Apache",
+        "GNU",
+        "MIT",
+        "BSD Simplified",
+        "BSD New",
+        "Boost",
+        "Creative",
+        "Eclipse",
+        "Mozilla",
+        "Unilicense",
+        "Other"
+    ]
+    },
+    //how to I add a badge to appear?
+    {
+        type:"input",
+        message:"Enter GitHun username.",
+        name:"name"
+    },
+    {
+        type:"input",
+        message:'Enter your email address.',
+        name: "email"
+    }
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
